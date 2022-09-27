563b3b3c98a43b808f19124e458edae70eda88fd
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-27 13:50:50 +0300

    03. пропуск элементов массива

---
+++ 03. пропуск элементов массива/js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// Пропуск элементов массива при деструктуризации в JavaScript
+/* №1⊗jsPmDstASE
+В следующем коде части массива записываются в соответствующие переменные:
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+
+let department = arr[2];
+let position   = arr[3];
+Переделайте этот код через деструктуризацию согласно изученной теории. */
+/* let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+let [, , department, position, ,] = arr; */
+
+
